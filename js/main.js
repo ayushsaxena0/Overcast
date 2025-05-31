833bd32472bb1561a2d35c0c096e786054bb9fcd
Ayush <ayushsaxena917@gmail.com>
2025-05-31 18:32:30 +0530

    Adding five day forecast

--- js/main.js
+++ js/main.js
@@ -1,117 +1,96 @@
-const searchCity = document.querySelector("input");
-searchCity.addEventListener("keypress", findWeather);
-
-let icons = [
-  {
-    icon: "01d",
-    img: "https://img.icons8.com/?size=100&id=15352&format=png&color=000000",
-  },
-  {
-    icon: "02d",
-    img: "https://img.icons8.com/?size=100&id=15359&format=png&color=000000",
-  },
-  {
-    icon: "03d",
-    img: "https://img.icons8.com/?size=100&id=39327&format=png&color=000000",
-  },
-  {
-    icon: "04d",
-    img: "https://img.icons8.com/?size=100&id=aAA5kxYkBsEm&format=png&color=000000",
-  },
-  {
-    icon: "09d",
-    img: "https://img.icons8.com/?size=100&id=15360&format=png&color=000000",
-  },
-  {
-    icon: "10d",
-    img: "https://img.icons8.com/?size=100&id=19541&format=png&color=000000",
-  },
-  {
-    icon: "11d",
-    img: "https://img.icons8.com/?size=100&id=39379&format=png&color=000000",
-  },
-  {
-    icon: "13d",
-    img: "https://img.icons8.com/?size=100&id=15356&format=png&color=000000",
-  },
-  {
-    icon: "50d",
-    img: "https://img.icons8.com/?size=100&id=obnXgS7SsY22&format=png&color=000000",
-  },
-  {
-    icon: "01n",
-    img: "https://img.icons8.com/?size=100&id=62034&format=png&color=000000",
-  },
-  {
-    icon: "02n",
-    img: "https://img.icons8.com/?size=100&id=VT8HlhlnhUwL&format=png&color=000000",
-  },
-  {
-    icon: "03n",
-    img: "https://img.icons8.com/?size=100&id=39327&format=png&color=000000",
-  },
-  {
-    icon: "04n",
-    img: "https://img.icons8.com/?size=100&id=aAA5kxYkBsEm&format=png&color=000000",
-  },
-  {
-    icon: "09n",
-    img: "https://img.icons8.com/?size=100&id=15360&format=png&color=000000",
-  },
-  {
-    icon: "10n",
-    img: "https://img.icons8.com/?size=100&id=wBPV56Uje50D&format=png&color=000000",
-  },
-  {
-    icon: "11n",
-    img: "https://img.icons8.com/?size=100&id=39379&format=png&color=000000",
-  },
-  {
-    icon: "13n",
-    img: "https://img.icons8.com/?size=100&id=15356&format=png&color=000000",
-  },
-  {
-    icon: "50n",
-    img: "https://img.icons8.com/?size=100&id=obnXgS7SsY22&format=png&color=000000",
-  },
-];
+document.querySelector("button").addEventListener("click", findWeather);
 
 async function findWeather(e) {
-  if (e.key === "Enter") {
-    const cityName = searchCity.value;
-    try {
-      const [{ lat, lon, name }] = await findCity(cityName);
-      const currentWeatherData = await getCurrentWeather(lat, lon);
-      const fiveDayWeatherData = await get5DayWeather(lat, lon);
-      console.log(currentWeatherData);
-      console.log(fiveDayWeatherData);
-      // set values
-      // main-section values
-      document.querySelector("#cityNameText").innerText = name;
-      document.querySelector("#tempText").innerText =
-        currentWeatherData.main.temp;
-      icons.forEach((el) => {
-        if (el.icon === currentWeatherData.weather[0].icon) {
-          document.querySelector("#main-img").src = el.img;
-        }
-      });
+  event.preventDefault();
+  const cityName = document.querySelector("input").value;
+  try {
+    const [{ lat, lon, name }] = await findCity(cityName);
+    const currentWeatherData = await getCurrentWeather(lat, lon);
+    const fiveDayWeatherData = await get5DayWeather(lat, lon);
 
-      // section2 values
-      for (let i = 0; i < 6; i++) {
-        const time = UnixToLocaleTime(fiveDayWeatherData.list[i].dt);
-        const card = `card${i + 1}`;
-        document.querySelector(`#${card}-time`).innerText = time;
-        document.querySelector(`#${card}-temp`).innerText =
-          fiveDayWeatherData.list[i].main.temp;
-        icons.forEach((el) => {
-          if (el.icon === fiveDayWeatherData.list[i].weather[0].icon) {
-            document.querySelector(`#${card}-img`).src = el.img;
-          }
-        });
-      }
-    } catch (error) {
-      console.error(error);
+    // showing container and removing instruction
+    document.querySelector("#instruction").classList.add("d-none");
+    document.querySelector("#container").classList.remove("d-none");
+    // set values
+    // main-section values
+    document.querySelector("#cityName").innerText = name;
+    document.querySelector("#tempText").innerText =
+      Math.floor(currentWeatherData.main.temp) + "°C";
+    document.querySelector("#weatherDesc").innerText =
+      currentWeatherData.weather[0].description.toUpperCase();
+
+    // section1 cards gird-one loop
+    let currentWeatherObj = {
+      "FEELS LIKE": Math.floor(currentWeatherData.main.feels_like) + "°C",
+      "CLOUDINESS (in %)": currentWeatherData.clouds.all,
+      "VISIBILITY (in m)": currentWeatherData.visibility,
+      HUMIDITY: currentWeatherData.main.humidity,
+    };
+
+    let gridOne = "";
+    for (const key in currentWeatherObj) {
+      gridOne += `<div class="col-6">
+                    <div class="card shadow-sm border-0">
+                      <div class="card-body">
+                        <p class="card-text">${key}</p>
+                        <h3 class="card-title">${currentWeatherObj[key]}</h3>
+                      </div>
+                    </div>
+                  </div>`;
     }
+
+    document.querySelector("#grid-one").innerHTML = gridOne;
+
+    // section2 card grid-two
+    let gridTwo = "";
+    for (let i = 0; i < 6; i++) {
+      const time = UnixToLocaleTime(fiveDayWeatherData.list[i].dt);
+      gridTwo += `<div class="col-6 col-sm-4 col-md-4 col-lg-2 ">
+                  <!-- inside card -->
+                  <div class="card shadow-sm border-0">
+                    <div class="card-body">
+                      <p class="card-text">${time[0]}</p>
+                      <h4 class="card-title">${Math.floor(
+                        fiveDayWeatherData.list[i].main.temp
+                      )}°C</h4>
+                      <img class="card-img-top img-fluid" src=
+                        'https://openweathermap.org/img/wn/${
+                          fiveDayWeatherData.list[i].weather[0].icon
+                        }@2x.png' 
+                      alt="Weather Icon" />
+                    </div>
+                  </div>
+                </div>`;
+    }
+    document.querySelector("#grid-two").innerHTML = gridTwo;
+
+    // section2 card grid-three
+    let gridThree = "";
+    let arr = [
+      fiveDayWeatherData.list[0],
+      fiveDayWeatherData.list[8],
+      fiveDayWeatherData.list[16],
+      fiveDayWeatherData.list[24],
+      fiveDayWeatherData.list[32],
+    ];
+    arr.forEach((el, i) => {
+      const day = UnixToLocaleTime(el.dt);
+      gridThree += `<div class="col-6 col-sm-4 col-md-4 col-lg-2">
+                  <div class="card border-0 shadow-sm ">
+                    <div class="card-body">
+                      <p class="card-text">${i == 0 ? "Today" : day[1]}</p>
+                      <h4 class="card-title">${Math.floor(el.main.temp)}°C</h4>
+                      <img class="card-img-top img-fluid" src='https://openweathermap.org/img/wn/${
+                        el.weather[0].icon
+                      }@2x.png' alt="Weather Icon" />
+                    </div>
+                  </div>
+                </div>`;
+    });
+
+    document.querySelector("#grid-three").innerHTML = gridThree;
+  } catch (error) {
+    console.error(error);
   }
 }
 
@@ -155,10 +134,16 @@ function UnixToLocaleTime(time) {
   const timestamp = time;
   const date = new Date(timestamp * 1000); // Convert to milliseconds
 
-  const timeString = date.toLocaleTimeString("en-US", {
+  const timeString = date.toLocaleTimeString("en-IN", {
+    timeZone: "Asia/Kolkata",
     hour: "2-digit",
     minute: "2-digit",
     hour12: true,
   });
-  return timeString;
+  const dayName = date.toLocaleDateString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    weekday: "short", // options: "long", "short", or "narrow"
+  });
+
+  return [timeString, dayName];
 }
